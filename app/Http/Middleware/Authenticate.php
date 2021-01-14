fcc5e5c1d20f5f6d804a6833b0c975c30d1c2d79
ilyas <ilyas@mewahniagajaya.com>
2021-01-14 14:24:55 +0700

    modify auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Exception;
 use Illuminate\Contracts\Auth\Factory as Auth;
 
 class Authenticate
@@ -35,8 +37,20 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if ($this->auth->guard($guard)->guest()) {
-            return response()->json(['result' => 'Unauthorized'] , 401);
+        // if ($this->auth->guard($guard)->guest()) {
+        //     return response()->json(['result' => 'Unauthorized'] , 401);
+        // }
+
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status' => 'Token is Invalid']);
+            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['status' => 'Token is Expired']);
+            }else{
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
         }
 
         return $next($request);
