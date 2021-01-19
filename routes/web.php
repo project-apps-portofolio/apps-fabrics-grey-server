4abc125c4872721e95e88b3d96cf20735c72df90
ilyas <ilyas@mewahniagajaya.com>
2021-01-19 09:59:59 +0700

    add header CORS

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 |
 */
 
-// header('Access-Control-Allow-Origin: *');
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
+header('Access-Control-Allow-Origin: *');
+// header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
 header( 'Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods:*');
 
